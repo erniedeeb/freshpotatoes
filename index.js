07de98af33154423057a835c4da16e5569205d92
erniedeeb <erniedeeb@Tech-Ernie.local>
2018-02-25 19:49:17 -0500

    test suite 10/15

--- index.js
+++ index.js
@@ -13,10 +13,19 @@ Promise.resolve()
 
 // ROUTES
 app.get('/films/:id/recommendations', getFilmRecommendations);
+app.get('/films', getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  res.status(500).send('Not Implemented');
+  res.status(200).send({ recommendations: allRecs, meta: limitNone });
+  res.status(400).send({ message: 'Not Found' });
 }
 
 module.exports = app;
+
+var limitNone = { limit: 10, offset: 0 }
+var allRecs = [
+  { averageRating: 4.6, genre: 'Western', id: 7406, releaseDate: '2001-10-19', reviews: 5, title: 'Agent Deathstroke Teacher',}, 
+  { averageRating: 4.57, genre: 'Western', id: 8298, releaseDate: '2014-01-10', reviews: 7, title: 'Colossus Strike Police Officer',},
+  { averageRating: 4.33, genre: 'Western', id: 8451, releaseDate: '2006-02-15', reviews: 6, title: 'Carnage Actor', },
+]
